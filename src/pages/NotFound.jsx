c21d6cec0be0c3c75b34b7df3cf00b1a8c97abab
carlfmichel <carlfmichel@gmail.com>
2025-05-24 18:57:44 -0400

    Added 404 Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { usePage } from "../context/PageContext";
+import fcpsLogo from "../assets/images/fcps-logo.svg";
 
 function NotFound() {
-  return <div>Not Found</div>;
+  const { setPageMeta } = usePage();
+
+  useEffect(() => {
+    setPageMeta({
+      hideComponent: true,
+    });
+  }, [setPageMeta]);
+
+  return (
+    <div className='page-error page'>
+      <div className='logo'>
+        <img src={fcpsLogo} alt='FCPS Logo' />
+      </div>
+      <div className='col-group'>
+        <div className='col'>
+          <span className='error404'>404</span>
+        </div>
+        <div className='col'>
+          <h1>Thie page does not exist</h1>
+          <Link to='/' className='btn'>
+            Go to home screen
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
