ab8e25724fd1054dc58d41fa8e1993d74fee2f7f
carlfmichel <carlfmichel@gmail.com>
2025-06-21 19:24:22 -0400

    add GA 2

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -1,12 +1,34 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+const GA_MEASUREMENT_ID = "G-1WPV2ZYM2Y";
+
 const usePageTracking = () => {
   const location = useLocation();
 
   useEffect(() => {
+    // Inject the GA script once on initial load
+    if (!window.gtag) {
+      const script = document.createElement("script");
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args) {
+        window.dataLayer.push(args);
+      }
+      window.gtag = gtag;
+
+      window.gtag("js", new Date());
+      window.gtag("config", GA_MEASUREMENT_ID);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Track page views on route changes
     if (window.gtag) {
-      window.gtag("config", "G-1WPV2ZYM2Y", {
+      window.gtag("config", GA_MEASUREMENT_ID, {
         page_path: location.pathname,
       });
     }
