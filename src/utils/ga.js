4a8fe4a5e3ea9b4af81fff972cd63874c5bc8a3e
carlfmichel <carlfmichel@gmail.com>
2025-07-03 17:59:23 -0400

    Turned off GA console

--- src/utils/ga.js
+++ src/utils/ga.js
@@ -1,7 +1,7 @@
 export const trackEvent = ({ action, ...params }) => {
   if (typeof window.gtag === "function") {
     window.gtag("event", action, params);
-    console.log(`[Analytics] ${action}`, params);
+    //console.log(`[Analytics] ${action}`, params);
   } else {
     console.warn("GA not ready:", action, params);
   }
